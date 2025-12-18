98669ac470605a05c4b2842c906928022527ce71
dargen3 <dargen3@centrum.cz>
2025-12-18 21:31:31 +0100

    Revert "update CPU"
    This reverts commit e24e74a26ad7534de4d0ffe0bdb06dacb8260d33.

--- api/proptimus-api/app/routes.py
+++ api/proptimus-api/app/routes.py
@@ -44,7 +44,7 @@ queue = Manager().list()
 running = Manager().list()
 optimisers = []
 number_of_processes = 1
-number_of_cpu = 59
+number_of_cpu = 1
 
 
 def get_interresidual_interactions(PDB_file):
